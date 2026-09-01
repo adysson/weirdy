ba4a7c429412d4cbd4c8b4b0f6edfd62928472c0
admin <adam@aidam.com>
2026-09-01 23:54:42 +0200

    Update functional tests to Rails 7 controller-test API

--- test/functional/weirdy/wexception_occurrences_controller_test.rb
+++ test/functional/weirdy/wexception_occurrences_controller_test.rb
@@ -2,10 +2,12 @@ require 'test_helper'
 
 module Weirdy
   class WexceptionOccurrencesControllerTest < ActionController::TestCase
+    setup { @routes = Weirdy::Engine.routes }
+
     test "should get wexception occurrences for a wexception" do
       wexception = create_wexception(RuntimeError, "Something is wrong")
       Weirdy::Config.auth = lambda { |controller| true }
-      xhr :get, :index, {wexception_id: wexception.id}, use_route: :weirdy
+      get :index, params: { wexception_id: wexception.id }, xhr: true
       assert_response :success
       assert_not_nil assigns(:wexception_occurrences)
       assert @response.body.include?(wexception.occurrences.first.message)
