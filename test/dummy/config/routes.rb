d54c5eb2f7ad7763dae13a494d5f785e259c62bf
admin <adam@aidam.com>
2026-09-01 22:47:14 +0200

    Modernize dummy app config to boot on Rails 7.1

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   mount Weirdy::Engine => "/weirdy"
-  match ':controller(/:action(/:id))(.:format)'
+  match ':controller(/:action(/:id))(.:format)', via: [:get, :post]
 end
