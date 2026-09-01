7e14014f38b4e9ead5c1cd1896ac372889fdbd19
admin <adam@aidam.com>
2026-09-01 23:54:42 +0200

    Precompile engine's own assets so views render under sprockets 4

--- lib/weirdy/engine.rb
+++ lib/weirdy/engine.rb
@@ -3,7 +3,7 @@ module Weirdy
     isolate_namespace Weirdy
 
     initializer "weirdy.assets.precompile" do |app|
-      app.config.assets.precompile += %w( application.js application.css )
+      app.config.assets.precompile += %w( weirdy/application.js weirdy/application.css )
     end
   end
 end
